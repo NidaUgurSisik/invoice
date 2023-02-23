cb23cb245acbfc4756ee1f379f82789d6fce6543
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-02-23 12:20:17 +0300

    a

--- main.py
+++ main.py
@@ -49,9 +49,7 @@ pdf_file = st.file_uploader("Upload PDF", type="pdf")
 # Convert PDF to JPG
 if pdf_file is not None:
     pdf_reader = PyPDF2.PdfReader(pdf_file)
-    page = pdf_reader.pages(0) # Get the first page
-    page_obj = page.to_page_output() # Convert the page to a page object
-    img = Image.open(page_obj)
+    img = Image.open(pdf_reader)
     img.save("page.jpg") # Save the image to a file
 
 else:
